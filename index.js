2fc7787c32172a4dcfed7aabc3e671ad4fc3f137
HuubMMIII <40438907+HuubMMIII@users.noreply.github.com>
2018-06-20 20:43:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-client.on('guildMemberAdd', member => {
+bot.on('guildMemberAdd', member => {
     member.send(':flag_nl: Welkom op de officiële Discord Server van iTournaments! Druk op de emoji onder het bericht in #taal-language om je taal te selecteren');
  });
 
- client.on('guildMemberAdd', member => {
+ bot.on('guildMemberAdd', member => {
     member.send('------------------------------------------------------------------------------------------');
  });
 
-client.on('guildMemberAdd', member => {
+bot.on('guildMemberAdd', member => {
     member.send(':flag_gb: Welcome to the official Discord Server of iTournaments! Click on the right emoji below the message at #taal-language to select your language');
  });
 
